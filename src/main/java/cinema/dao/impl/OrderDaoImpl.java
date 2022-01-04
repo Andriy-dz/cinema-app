a3345192552c3037be243d8c1c7347556baf5248
Andriy-dz <andriydzdz@gmail.com>
2022-01-04 14:41:00 +0200

    removed empty line

--- src/main/java/cinema/dao/impl/OrderDaoImpl.java
+++ src/main/java/cinema/dao/impl/OrderDaoImpl.java
@@ -13,7 +13,6 @@ import org.hibernate.query.Query;
 
 @Dao
 public class OrderDaoImpl implements OrderDao {
-
     @Override
     public Order add(Order order) {
         Transaction transaction = null;
@@ -39,7 +38,7 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Order> query = session.createQuery("select DISTINCT FROM Order o "
+            Query<Order> query = session.createQuery("SELECT DISTINCT o FROM Order o "
                     + "LEFT JOIN FETCH o.tickets t "
                     + "LEFT JOIN FETCH t.user u "
                     + "LEFT JOIN FETCH t.movieSession ms "
